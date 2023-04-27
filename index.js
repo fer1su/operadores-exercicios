450a0a5c39d1a82d7be1d8006e1df32369cb3d0e
fer1su <rafael.rodrigues022@hotmail.com>
2023-04-26 21:22:01 -0300

    exercicios operadores

---
+++ index.js
@@ -0,0 +1,64 @@
+//Na primeira atividade a ordem de impressão é a seguinte False, False, True, true
+
+//Na segunda atividade faltou atribuir Number para fazer o calculo
+
+//Na terceira atividade basta sugerir atribuir Number para o prompt
+//ficando let primeiroNumero = Number(prompt("Digite um numero!"))
+
+// const idade = Number(prompt("Qual a suda idade:"))
+// const idade2 = Number(prompt("Qual a idade do seu amigo(a):"))
+// console.log (idade >= idade2)
+// console.log (idade - idade2)
+
+// const par = Number(prompt("Insira um número par:"))
+// console.log (par/2)
+//O número sempre acaba como par
+//O número sempre acaba com virgula
+
+// const idade = Number(prompt("Insira sua idade"))
+// const meses = idade * 12
+// const dias = meses * 365
+// const horas = dias * 24
+// console.log (meses, dias, horas)
+
+// const numero1 = Number(prompt("Insira o primeiro número"))
+// const numero2 = Number(prompt("Insira o segundo número"))
+// console.log (numero1 > numero2)
+// console.log (numero1 === numero2)
+// console.log(numero1 % 2 === 0)
+// console.log(numero2 % 2 === 0)
+
+//Atividade A
+// const f = 77
+// const k = ((77-32)*(5/9)+273.15)
+// console.log(k)
+
+//Atividade B
+// const c = 80
+// const f1 = ((80)*(9/5)+32)
+// console.log(f1)
+
+//Atividade C
+// const c1 = 30
+// const f2 = ((30)*(9/5)+32)
+// const k1 = ((f2-32)*(5/9)+273.15)
+// console.log (f2,k1)
+
+//Atividade D
+// const c1 = Number(prompt("Insira a temperatura hoje (ºC):"))
+// const f2 = ((30)*(9/5)+32)
+// const k1 = ((f2-32)*(5/9)+273.15)
+// console.log (f2,k1)
+
+//Atividade 2 A
+// const r = 0.05
+// const q = 280
+// console.log (r * q)
+
+// 2 B
+// const r = 0.05
+// const q = 280
+// const d = 0.05 * 280
+// const d1 = 15
+// console.log(d%d1)
+
